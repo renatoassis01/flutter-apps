58eaa01fc36338f772229da26c73ea70d2358809
Renato Assis <renatoweverton@gmail.com>
2020-10-10 11:20:35 -0300

    46. TextField e Decoration

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -21,6 +21,32 @@ class _AppState extends State<App> {
           IconButton(icon: Icon(Icons.refresh), onPressed: () {})
         ],
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(
+            Icons.person_outline,
+            size: 120.0,
+            color: Colors.green,
+          ),
+          TextField(
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Peso(kg)",
+                labelStyle: TextStyle(color: Colors.green)),
+          ),
+          TextField(
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Altura(cm)",
+                labelStyle: TextStyle(color: Colors.green)),
+          )
+        ],
+      ),
     );
   }
 }
